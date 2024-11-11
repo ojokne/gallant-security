57933283d9725cb373d5d5d67dcda1306ba7f2e2
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-11-11 04:43:37 +0300

    Create visually-hidden.jsx

---
+++ components/common/visually-hidden.jsx
@@ -0,0 +1,7 @@
+export function VisuallyHidden({ children }) {
+  return (
+    <span className="absolute w-px h-px p-0 -m-px overflow-hidden whitespace-nowrap border-0">
+      {children}
+    </span>
+  );
+} 
